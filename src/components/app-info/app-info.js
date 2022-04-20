3ae42e6089ec2ea1a4a61d017e8ecfdd201b996e
AlisaCosmos <57442445+AlisaCosmos@users.noreply.github.com>
2022-04-20 15:30:58 +0300

    Outline the structure of the application

---
+++ src/components/app-info/app-info.js
@@ -0,0 +1,13 @@
+
+import './app-info.css';
+
+const AppInfo = () => {
+    return (
+        <div>
+            <h1>React приложение для изучения иностранных слов по карточкам</h1>
+        </div>
+        
+    )
+}
+
+export default AppInfo;
